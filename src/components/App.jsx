99cd3e09bbd473844a832995b73d53e0c0b91cfa
IsaienkoViktor <isaenkov1986@gmail.com>
2023-06-28 17:17:27 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
-// test
+import { getImages } from '../Service/ImageApi.js';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+    images: [],
+    showBtn: false,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page, query } = this.state;
+    if (prevState.page !== page || prevState.query !== query) {
+      getImages(query, page).then(({ photos, total_results }) => {
+        console.log(photos);
+        this.setState(prevState => ({
+          images: [...prevState.images, ...photos],
+          showBtn: page < Math.ceil(total_results / 15),
+        }));
+      });
+    }
+  }
+
+  handelSubmit = query => {
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+      showBtn: false,
+    });
+  };
+  onClickButton = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <button>search</button>
+      </form>
+    );
+  }
+}
